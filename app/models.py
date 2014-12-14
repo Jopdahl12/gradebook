2888d797dfdafb05ea376e8aa77db370485fc813
Jopdahl12 <opdajo01@luther.edu>
2014-12-13 23:59:57 -0600

    First try to implement Classroom class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,42 @@
 from app import db
 
-classroom = db.Table('classroom',
-	db.Column("student", db.String(8), db.ForeignKey('student.id')),
-	db.Column("course", db.Integer, db.ForeignKey('course.name'))
-)
+class Classroom(db.Model):
+	__tablename__ = 'classroom'
+	student_id = db.Column(db.String(8), db.ForeignKey('student.id'), primary_key=True)
+	course_name = db.Column(db.Integer, db.ForeignKey('course.name'), primary_key=True)
+	overall_grade = db.Column(db.String(1))
+	possible = db.Column(db.Integer)
+	grade = db.Column(db.Float)
+	course = db.relationship("Course", backref="stud_ass")
+	assignments = db.relationship("Assignment", backref="for_class", lazy='dynamic')
+
+	def update_possible(self):
+		for assignment in assignments:
+			self.possible+=assignment.out_of
+		return self
+
+	def update_grade(self):
+		total_points = 0
+		for assignment in assignments:
+			total_points+=assignment.total
+		self.grade = total_points/self.possible
+		return self
+
+	def update_overall_grade(self):
+		if self.grade >= 90:
+			self.overall_grade='A'
+		elif self.grade >= 80:
+			self.overall_grade='B'
+		elif self.grade >= 70:
+			self.overall_grade='C'
+		elif self.grade >= 60:
+			self.overall_grade='D'
+		else: 
+			self.overall_grade='F'
+		return self
+
+	def __repr__(self):
+		return (self.student.first_name + self.student.last_name + ' ' + self.course.name)
 
 class Student(db.Model):
 	id = db.Column(db.String(8), primary_key=True)
@@ -13,11 +46,8 @@ class Student(db.Model):
 	year = db.Column(db.Integer)
 	total_credits = db.Column(db.Integer)
 	student_pass = db.Column(db.String(50))
-	classes = db.relationship("Course",
-							secondary= classroom,
-							primaryjoin=(classroom.c.student == id),
-							backref=db.backref("students", lazy='dynamic'),
-							lazy='dynamic')
+	classes = db.relationship("Classroom",
+							backref="student")
 
 	def is_authenticated(self):
 		return True
@@ -91,9 +121,8 @@ class Admin(db.Model):
 class Course(db.Model):
 	course_num = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80), index=True, unique=True)
-	# student_id = db.Column(db.String(8), db.ForeignKey('student.id'))
 	admin_id = db.Column(db.String(8), db.ForeignKey('admin.id'))
-	assignments = db.relationship("Assignment", backref="for_class", lazy='dynamic')
+
 
 	def is_empty(self):
 		if self.assignments is None:
@@ -108,7 +137,7 @@ class Assignment(db.Model):
 	name = db.Column(db.String(100), index=True)
 	total = db.Column(db.Float)
 	out_of = db.Column(db.Integer)
-	course = db.Column(db.Integer, db.ForeignKey('course.course_num'))
+	course = db.Column(db.Integer, db.ForeignKey('classroom.course_name'))
 
 	def score(self):
 		score = self.total/self.out_of
